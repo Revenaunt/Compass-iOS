485b88294923f0444db87a5229515ae11e994860
Ismael Alonso <ismaha91@gmail.com>
2016-04-11 12:44:19 -0500

    Add temp load/button switch, rename ViewController to LauncherViewController

---
+++ Compass/LauncherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Compass
+//
+//  Created by Ismael Alonso on 4/7/16.
+//  Copyright © 2016 Tennessee Data Commons. All rights reserved.
+//
+
+import UIKit
+
+class LauncherViewController: UIViewController {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var logIn: UIButton!
+
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        let defaults = NSUserDefaults.standardUserDefaults();
+        if !defaults.boolForKey("hasLoggedIn"){
+            activityIndicator.hidden = true;
+            signUp.hidden = false;
+            logIn.hidden = false;
+        }
+    }
+
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func signUpClick(){
+        
+    }
+    
+    @IBAction func logInClick(){
+        
+    }
+    
+}
+
