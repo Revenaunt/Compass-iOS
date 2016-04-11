28611704e694222eb6a06a5eb9cf6b1bd3d3a839
Ismael Alonso <ismaha91@gmail.com>
2016-04-11 11:41:52 -0500

    Configure Carthage and import Locksmith to easily access the keychain

--- Compass/ViewController.swift
+++ Compass/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var logIn: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
