d7aa74a1ca998bb7d30eba93df23bbc330f89f65
Ismael Alonso <ismaha91@gmail.com>
2016-04-12 15:49:59 -0500

    Finish Sign Up layout

--- Compass/SignUpViewController.swift
+++ Compass/SignUpViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class SignUpViewController: UIViewController{
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad();
+        
+        signUpButton.backgroundColor = UIColor.init(red: 0.1, green: 0.46, blue: 0.82, alpha: 1);
+        signUpButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal);
+        signUpButton.layer.cornerRadius = 18;
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool){
         super.viewWillAppear(animated);
         navigationController?.setNavigationBarHidden(false, animated: animated);
     }
