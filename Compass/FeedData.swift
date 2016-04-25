8cd53f316b16f4c5accb7f9fe50ca51f8fa81424
Ismael Alonso <ismaha91@gmail.com>
2016-04-25 16:10:43 -0500

    Add a basic FeedData model class

---
+++ Compass/FeedData.swift
@@ -0,0 +1,120 @@
+//
+//  FeedData.swift
+//  Compass
+//
+//  Created by Ismael Alonso on 4/25/16.
+//  Copyright © 2016 Tennessee Data Commons. All rights reserved.
+//
+
+import ObjectMapper
+
+
+//No need to extend TDCBase
+class FeedData: Mappable{
+    private var progress: Progress? = nil;
+    private var actionFeedback: ActionFeedback? = nil;
+    
+    
+    required init?(_ map: Map){
+        
+    }
+    
+    func mapping(map: Map){
+        progress <- map["progress"];
+        actionFeedback <- map["action_feedback"];
+    }
+    
+    
+    
+    private class Progress: Mappable{
+        private var totalActions: Int = 0;
+        private var completedActions: Int = 0;
+        private var progressPercentage: Int = 0;
+        
+        
+        required init?(_ map: Map){
+            
+        }
+        
+        private func mapping(map: Map){
+            totalActions <- map["total"];
+            completedActions <- map["completed"];
+            progressPercentage <- map["progress"];
+        }
+        
+        func complete(){
+            completedActions += 1;
+            progressPercentage = completedActions * 100 / totalActions;
+        }
+        
+        func remove(){
+            totalActions -= 1;
+            progressPercentage = completedActions * 100 / totalActions;
+        }
+        
+        func getTotalActions() -> Int{
+            return totalActions;
+        }
+        
+        func getCompletedActions() -> Int{
+            return completedActions;
+        }
+        
+        func getProgressPercentage() -> Int{
+            return progressPercentage;
+        }
+        
+        func getProgressFraction() -> String{
+            return "\(completedActions)/\(totalActions)";
+        }
+    }
+    
+    private class ActionFeedback: Mappable{
+        private var title: String = "";
+        private var subtitle: String = "";
+        private var iconId: Int = -1;
+        
+        private var goalId: Int = -1;
+        private var goalType: String = "";
+        
+        
+        required init?(_ map: Map){
+            
+        }
+        
+        private func mapping(map: Map){
+            title <- map["title"];
+            subtitle <- map["subtitle"];
+            iconId <- map["icon"];
+        }
+        
+        func getTitle() -> String{
+            return title;
+        }
+        
+        func getSubtitle() -> String{
+            return subtitle;
+        }
+        
+        func getIconResource() -> String{
+            //TODO
+            return "";
+        }
+        
+        func setGoal(/* UpcomingAction */){
+            //TODO
+        }
+        
+        func getGoalId() -> Int{
+            return goalId;
+        }
+        
+        func hasUserGoal() -> Bool{
+            return goalType == "usergoal";
+        }
+        
+        func hasCustomGoal() -> Bool{
+            return goalType == "customgoal";
+        }
+    }
+}
