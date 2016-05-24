503914fb0de8a2fd9b43bb797ea4ea96eb10986c
Ismael Alonso <ismaha91@gmail.com>
2016-05-24 10:51:24 -0500

    Fix the GoalController layout and populate goal and category related fields

--- Compass/GoalViewController.swift
+++ Compass/GoalViewController.swift
@@ -6,11 +6,40 @@
 //  Copyright © 2016 Tennessee Data Commons. All rights reserved.
 //
 
-import Foundation
+import Nuke
 
 
 class GoalViewController: UIViewController{
-    var category: CategoryContent? = nil;
-    var goal: GoalContent? = nil;
+    //Data
+    var category: CategoryContent?;
+    var goal: GoalContent?;
     
+    //UI components
+    @IBOutlet weak var header: UIView!
+    @IBOutlet weak var headerImage: UIImageView!
+    
+    @IBOutlet weak var goalTitle: UILabel!
+    @IBOutlet weak var goalDescription: UILabel!
+    
+    @IBOutlet weak var additionalSectionTitle: UILabel!
+    @IBOutlet weak var additionalSectionContent: UILabel!
+    
+    
+    override func viewDidLoad(){
+        //Background color of the header
+        header.layer.backgroundColor = category!.getParsedColor().CGColor;
+        
+        //Header image
+        if (category!.getIconUrl().characters.count != 0){
+            Nuke.taskWith(NSURL(string: category!.getIconUrl())!){
+                let image = $0.image;
+                self.headerImage.layer.cornerRadius = self.headerImage.frame.height/2;
+                self.headerImage.clipsToBounds = true;
+                self.headerImage.image = image;
+                }.resume();
+        }
+        
+        goalTitle.text = goal?.getTitle();
+        goalDescription.text = goal?.getDescription();
+    }
 }
