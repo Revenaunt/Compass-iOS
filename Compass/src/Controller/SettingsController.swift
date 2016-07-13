407129f397307aa162b30d47c7d0b6f99b0d16bd
Ismael Alonso <ismaha91@gmail.com>
2016-07-13 13:02:36 -0500

    Populate settings and add functionality to tos, privacy and sign out

---
+++ Compass/src/Controller/SettingsController.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsController.swift
+//  Compass
+//
+//  Created by Ismael Alonso on 7/13/16.
+//  Copyright © 2016 Tennessee Data Commons. All rights reserved.
+//
+
+import UIKit
+import Locksmith
+
+
+class SettingsController: UITableViewController{
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        if (indexPath.section == 0){
+            print("General");
+            if (indexPath.row == 0){
+                
+            }
+            else if (indexPath.row == 1){
+                do{
+                    //Remove the user info
+                    try Locksmith.deleteDataForUserAccount("CompassAccount");
+                    //Back to the login screen
+                    let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil);
+                    let viewController = mainStoryboard.instantiateViewControllerWithIdentifier("LauncherNavController");
+                    UIApplication.sharedApplication().keyWindow?.rootViewController = viewController;
+                }
+                catch{
+                    
+                }
+            }
+        }
+        else if (indexPath.section == 1){
+            print("About");
+            if (indexPath.row == 0){
+                UIApplication.sharedApplication().openURL(NSURL(string: "https://app.tndata.org/terms/")!);
+            }
+            else if (indexPath.row == 1){
+                UIApplication.sharedApplication().openURL(NSURL(string: "https://app.tndata.org/privacy/")!);
+            }
+        }
+    }
+}
