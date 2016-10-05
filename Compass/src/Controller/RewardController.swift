d2f176b25148f950045b9991aa52407dda2b38aa
Ismael Alonso <ismaha91@gmail.com>
2016-10-05 13:07:04 -0500

    Get scroll view to work properly and add and populate content card

--- Compass/src/Controller/RewardController.swift
+++ Compass/src/Controller/RewardController.swift
@@ -13,23 +13,72 @@ import ObjectMapper
 
 class RewardController: UIViewController{
     @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var contentCard: UIView!
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet var author: UILabel!
     
     private var reward: Reward? = nil;
+    private var authorConstraints = [NSLayoutConstraint]();
     
     
     override func viewDidLoad(){
+        contentCard.layer.cornerRadius = 4;
+        removeAuthorLabel();
         if (reward == nil){
-            Just.get(API.getRandomRewardUrl()){ (response) in
-                if (response.ok){
-                    let result = String(data: response.content!, encoding:NSUTF8StringEncoding)!;
-                    self.reward = Mapper<ParserModels.RewardArray>().map(result)?.rewards![0];
-                    print(self.reward!);
-                    dispatch_async(dispatch_get_main_queue(), {
-                        self.icon.layoutIfNeeded();
-                        self.icon.image = self.reward?.getImageAsset();
-                    });
+            fetchReward();
+        }
+        else{
+            setReward(reward!);
+        }
+    }
+    
+    private func removeAuthorLabel(){
+        if authorConstraints.isEmpty{
+            for constraint in contentCard.constraints{
+                if constraint.belongsTo(author){
+                    authorConstraints.append(constraint);
                 }
             }
+            author.removeFromSuperview();
+            contentCard.setNeedsLayout();
+            contentCard.layoutIfNeeded();
+        }
+    }
+    
+    private func addAuthorLabel(){
+        if !authorConstraints.isEmpty{
+            contentCard.addSubview(author);
+            for constraint in authorConstraints{
+                contentCard.addConstraint(constraint);
+            }
+            authorConstraints.removeAll();
+            contentCard.setNeedsLayout();
+            contentCard.layoutIfNeeded();
+        }
+    }
+    
+    private func fetchReward(){
+        removeAuthorLabel();
+        Just.get(API.getRandomRewardUrl()){ (response) in
+            if (response.ok){
+                let result = String(data: response.content!, encoding:NSUTF8StringEncoding)!;
+                let reward = Mapper<ParserModels.RewardArray>().map(result)?.rewards![0];
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.setReward(reward!);
+                });
+            }
+        }
+    }
+    
+    private func setReward(reward: Reward){
+        self.reward = reward;
+        icon.image = reward.getImageAsset();
+        header.text = reward.getHeaderTitle();
+        message.text = reward.getMessage();
+        if reward.isQuote(){
+            addAuthorLabel();
+            author.text = "-\(reward.getAuthor())";
         }
     }
 }
