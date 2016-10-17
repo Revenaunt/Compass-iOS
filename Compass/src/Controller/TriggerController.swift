8c232a2fa64646a5323bb75183127f2a21b5b68e
Ismael Alonso <ismaha91@gmail.com>
2016-10-17 16:14:41 -0500

    Add trigger editor layout

---
+++ Compass/src/Controller/TriggerController.swift
@@ -0,0 +1,14 @@
+//
+//  TriggerController.swift
+//  Compass
+//
+//  Created by Ismael Alonso on 10/17/16.
+//  Copyright © 2016 Tennessee Data Commons. All rights reserved.
+//
+
+import UIKit
+
+
+class TriggerController: UIViewController{
+    
+}
